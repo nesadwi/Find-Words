d3d14b3e6998f5195d28464f26758bfad596c4a2
azhari1070 <38501420+azhari1070@users.noreply.github.com>
2021-05-02 22:13:53 +0700

    Create FindwordKelompok3.cpp
    tolong lanjutin, masih saya coba untuk bisa langsung inpu input input input, cuma blum nemu caranya

--- FindwordKelompok3.cpp
+++ FindwordKelompok3.cpp
@@ -3,60 +3,28 @@
 //2017051032 - Mesak Tanati
 #include <iostream>
 #include <cstring>
-#include <cstdlib>   
+#include <algorithm>
 using namespace std;
-int main(){
-	char kata2[15];
-	int cari,panjang,flag,flag2;
-	char kata[15][15] =			{{'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
-						{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
-						{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
-						{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'},
-						{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'},
-						{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'},
-						{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'},
-						{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'},
-						{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'},
-						{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'},
-						{'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'},
-						{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'},
-						{'m','o','s','g','z','c','z','d','t','d','b','o','o','t','o'},
-						{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
-						{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'}};
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-	for (int i = 0; i < 15; i++)
-    {
-        check = strstr(words[i], input);
-        
-	    if(check != NULL){
-	        return true;
-	    }
-    }
-    
-    return false;
-}
+const int cols = 16, rows = 15;
+ char words[rows][cols] = {"tgbwwinterwsesn",
+                        	"aaunttmmhfoodnb",
+                            "jlwcqldzmpmvdmr",
+                            "asagmquwvvbsohi",
+                            "bwplotanadtpgnc",
+                            "rewngodjcpnatnk",
+                            "eeotwosbqharrsa",
+                            "azcgeswewnaknpb",
+                            "dinnerqodlwdcar",
+                            "onopkwmparktzcc",
+                            "qbfrogmamwpweey",
+                            "lqzqnnmrzjjsclg",
+                            "mosgzczetdbooto",
+                            "pdcrzmsngrdnrpz",
+                            "ohnkzwaterjgtra"};
 
-bool searchReverseVertical(char input[]){ //Nyari Secari Vertikal
+char *getWordVertical(int );
+char *reverse(char *);
+bool searchVertical(char input[]){
 	char *check;
 	char kata[15];
 	
@@ -67,7 +35,6 @@ bool searchReverseVertical(char input[]){ //Nyari Secari Vertikal
             kata[j] = words[j][i];
         }
         
-        reverse(kata, kata + strlen(kata)); //huruf di balik menggunakan reverse
         check = strstr(kata, input);
 
         if(check != NULL){
@@ -77,17 +44,28 @@ bool searchReverseVertical(char input[]){ //Nyari Secari Vertikal
     
     return false;
 }
-
-bool searchReverseHorizontal(char input[]){ //Nyari Secara Horizontal
+bool searchHorizontal(char input[]){
 	char *check;
-	char kata[15]; //Menyimpan Huruf Acak
 	
-	for (int i = 0; i < 15; i++)
-    {
-    	for(int j = 0; j < 15; j++){
-	    	kata[j] = words[i][j];
-		}
-		  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
